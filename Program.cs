cc9322f0d2334ac05665cfe9440695baf0f1cea9
whit oswald <whit.augustus@gmail.com>
2023-03-21 07:15:53 -0400

    simple code formatting changes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
-using System.ComponentModel.Design;
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
 
 namespace NumberGuesser
 {
@@ -11,52 +6,48 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            // print introductory lines to console
             WriteLineInColor("Number Guesser v1.0.0 by Whitten Oswald", ConsoleColor.Yellow);
             Console.Write("What is your name? : ");
             Console.WriteLine("Hello {0}, let's play a game...", Console.ReadLine());
+
+            // initialize variables that will be consistent thru all rounds
             int correct = 0, attempts = 0;
 
-
-            while (true)
-            {
+            while (true) {
+                // initialize variables that will reset after each round
                 Random random = new Random();
-
-                int correctNumber = random.Next(1, 10);
-                int guess = 0;
+                int correctNumber = random.Next(1, 10), guess = 0;
                 Console.WriteLine("Guess a number between 1 and 10");
 
-                while (guess != correctNumber)
-                {
+                while (guess != correctNumber) {
                     attempts++;
                     string input = Console.ReadLine();
 
-                    if (!int.TryParse(input, out guess))
-                    {
+                    // error checking: ensure user's input is numerical
+                    if (!int.TryParse(input, out guess)) {
                         WriteLineInColor("Please enter a digit.", ConsoleColor.Red);
                         continue;
                     }
 
+                    // check whether guess is correct
                     guess = Int32.Parse(input);
-
-                    if (guess != correctNumber)
-                    {
+                    if (guess != correctNumber) {
                         WriteLineInColor("Wrong number. Guess again", ConsoleColor.Red);
                     }
-
                 }
                 correct++;
                 WriteLineInColor("CORRECT!!", ConsoleColor.DarkYellow);
                 Console.WriteLine("Play again? [Y / N]");
                 string answer = Console.ReadLine().ToUpper();
-                if (answer == "Y")
-                {
+                if (answer == "Y") {
                     continue;
                 }
-                else
-                {
+                else {
+                    // calculate and print accuracy over all games played
                     WriteLineInColor("Game over", ConsoleColor.Red);
                     double percent = (double)correct / attempts;
-                    Console.WriteLine("Your Accuracy ==> {0} / {1} {2}%", correct, attempts, percent);
+                    Console.WriteLine("Your Accuracy ==> {0} / {1} = {2}%", correct, attempts, percent);
                     return;
                 }
             }
